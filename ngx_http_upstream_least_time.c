ad15f937b67e213bb0fe8e98cd5bc82a4463ae43
Artemiev Igor <urgor-deadbeef@narod.ru>
2026-04-02 13:12:59 +0300

    Fix some bugs with nginx 1.28.0 Add health check collaboration

--- ngx_http_upstream_least_time.c
+++ ngx_http_upstream_least_time.c
@@ -3,24 +3,26 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
+#if (NGX_HTTP_UPSTREAM_CHECK)
+#include "ngx_http_upstream_check_module.h"
+#endif
+
 #include "ngx_http_upstream_least_time.h"
+
+#define ngx_http_upstream_tries(p) ((p)->tries                                \
+                                    + ((p)->next ? (p)->next->tries : 0))
 
 
 static char *ngx_http_upstream_least_time(ngx_conf_t *cf, ngx_command_t *cmd, 
     void *conf);
-#if (NGX_HTTP_UPSTREAM_ZONE)
-static ngx_int_t ngx_http_upstream_least_time_init_zone(
-    ngx_shm_zone_t *shm_zone, void *data);
-#endif
 
 #if (NGX_HTTP_SSL)
-
 static ngx_int_t ngx_http_upstream_empty_set_session(ngx_peer_connection_t *pc,
     void *data);
 static void ngx_http_upstream_empty_save_session(ngx_peer_connection_t *pc,
     void *data);
-
 #endif
+
 static void *ngx_http_upstream_least_time_create_conf(ngx_conf_t *cf); 
 
 
@@ -81,109 +83,19 @@ ngx_http_upstream_least_time_create_conf(ngx_conf_t *cf)
     return conf;
 }   
 
-
-static void
-ngx_http_upstream_least_time_ref_peers(ngx_http_upstream_least_time_peers_t  *peers,
-	ngx_http_upstream_rr_peers_t  *rrpeers)
-{
-    ngx_http_upstream_least_time_peer_t   *peer, **peerp;
-    ngx_http_upstream_rr_peer_t		  *rrpeer;
-    ngx_uint_t				   n;
-
-    peer = peers->peer;
-
-    peers->rr = rrpeers;
-    peerp = &peers->peer;
-    rrpeer = rrpeers->peer;
-
-    for (n = 0; n < rrpeers->number; n++) {
-	peer[n].rr = rrpeer;
-
-	*peerp = &peer[n];
-        peerp = &peer[n].next;
-	rrpeer = rrpeer->next;
-    }
-}
-
+#define GET_AVG_TIME(p) ((ngx_msec_t)((p)->spare[0]))
+#define SET_AVG_TIME(p, t) (p)->spare[0] = t
 
 ngx_int_t
 ngx_http_upstream_init_least_time(ngx_conf_t *cf,
     ngx_http_upstream_srv_conf_t *us)
 {
-    ngx_uint_t                     n;
-    ngx_http_upstream_server_t    *server;
-    ngx_http_upstream_least_time_peer_t   *peer, **peerp;
-    ngx_http_upstream_least_time_peers_t  *peers, *backup;
-    
-    ngx_http_upstream_rr_peers_t  *rrpeers, *rrbackup;
-    ngx_http_upstream_rr_peer_t  *rrpeer;
     
     if (ngx_http_upstream_init_round_robin(cf, us) != NGX_OK) {
         return NGX_ERROR;
     }
 
-
     us->peer.init = ngx_http_upstream_init_least_time_peer;
-    rrpeers = us->peer.data;
-
-    peers = ngx_pcalloc(cf->pool, sizeof(ngx_http_upstream_least_time_peers_t));
-    if (peers == NULL) {
-	return NGX_ERROR;
-    }
-
-    peer = ngx_pcalloc(cf->pool, sizeof(ngx_http_upstream_least_time_peer_t) * rrpeers->number);
-    if (peer == NULL) {
-	return NGX_ERROR;
-    }
-
-#if (NGX_HTTP_UPSTREAM_ZONE)
-    ngx_http_upstream_main_conf_t  *umcf = ngx_http_conf_get_module_main_conf(
-	cf, ngx_http_upstream_module);
-
-    if (us->shm_zone) {
-	if(us->shm_zone->init && us->shm_zone->data) {
-	    ngx_http_upstream_least_time_zone_t *z = ngx_pcalloc(cf->pool, sizeof(ngx_http_upstream_least_time_zone_t));
-	    if (NULL == z) {
-		return NGX_ERROR;
-	    }
-	    
-	    z->init = us->shm_zone->init;
-	    z->data = us->shm_zone->data;
-	    z->u = us;
-	    z->cf = umcf;
-	    z->peers = peers;
-
-	    us->shm_zone->init = ngx_http_upstream_least_time_init_zone;
-	    us->shm_zone->data = z;
-	}
-    }
-#endif    
-    
-    peers->peer = peer;
-    ngx_http_upstream_least_time_ref_peers(peers, rrpeers);
-    us->peer.data = peers;
-
-    /* backup servers */
-
-    rrbackup = rrpeers->next;
-
-    if (!rrbackup) {
-	return NGX_OK;
-    }
-
-    backup = ngx_pcalloc(cf->pool, sizeof(ngx_http_upstream_least_time_peers_t));
-    if (backup == NULL) {
-	return NGX_ERROR;
-    }
-
-    peer = ngx_pcalloc(cf->pool, sizeof(ngx_http_upstream_least_time_peer_t) * rrbackup->number);
-    if (peer == NULL) {
-	return NGX_ERROR;
-    }
-    
-    backup->peer = peer;
-    ngx_http_upstream_least_time_ref_peers(backup, rrbackup);
-    peers->next = backup;
 
     return NGX_OK;
 }
@@ -193,31 +105,20 @@ ngx_int_t
 ngx_http_upstream_init_least_time_peer(ngx_http_request_t *r,
     ngx_http_upstream_srv_conf_t *us)
 {
-    ngx_uint_t				       n;
-    ngx_http_upstream_least_time_peer_data_t  *ltp = r->upstream->peer.data;
-
-    if (NULL == ltp) {
-	ltp = ngx_palloc(r->pool, sizeof(ngx_http_upstream_least_time_peer_data_t));
-	if (NULL == ltp) {
-	    return NGX_ERROR;
-	}
-	ltp->rrp = NULL;
-    } 
+    ngx_http_upstream_least_time_peer_data_t *data = NULL;
     
-    r->upstream->peer.data = ltp->rrp;
-    ltp->peers = us->peer.data;
-    us->peer.data = ltp->peers->rr;
-
     if (ngx_http_upstream_init_round_robin_peer(r, us) != NGX_OK) {
         return NGX_ERROR;
     }
-       
-    ltp->rrp = r->upstream->peer.data;
-    r->upstream->peer.data = ltp;
+    data = ngx_palloc(r->pool, sizeof(ngx_http_upstream_least_time_peer_data_t));
 
-    ltp->current = NULL;
-    ltp->request = r;
-    us->peer.data = ltp->peers;
+    if (NULL == data) {
+	return NGX_ERROR;
+    }
+
+    data->request = r;
+    data->rrp = r->upstream->peer.data;
+    r->upstream->peer.data = data;
 
     r->upstream->peer.get = ngx_http_upstream_get_least_time_peer;
     r->upstream->peer.free = ngx_http_upstream_free_least_time_peer;
@@ -228,15 +129,15 @@ ngx_http_upstream_init_least_time_peer(ngx_http_request_t *r,
 static ngx_int_t
 ngx_http_upstream_get_least_time_peer(ngx_peer_connection_t *pc, void *data)
 {
-    ngx_http_upstream_least_time_peer_data_t  *ltp = data;
-    ngx_http_upstream_rr_peer_data_t  *rrp = ltp->rrp;
+    ngx_http_upstream_least_time_peer_data_t *d = data;
+    ngx_http_upstream_rr_peer_data_t  *rrp = d->rrp;
 
     time_t                         now;
     uintptr_t                      m;
     ngx_int_t                      rc, total;
     ngx_uint_t                     i, n, p, many;
-    ngx_http_upstream_least_time_peer_t   *peer, *best;
-    ngx_http_upstream_least_time_peers_t  *peers;
+    ngx_http_upstream_rr_peer_t   *peer, *best;
+    ngx_http_upstream_rr_peers_t  *peers;
 
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0,
                    "get least time peer, try: %ui", pc->tries);
@@ -250,9 +151,15 @@ ngx_http_upstream_get_least_time_peer(ngx_peer_connection_t *pc, void *data)
 
     now = ngx_time();
 
-    peers = ltp->peers;
+    peers = rrp->peers;
 
-    ngx_http_upstream_rr_peers_wlock(peers->rr);
+    ngx_http_upstream_rr_peers_wlock(peers);
+
+#if (NGX_HTTP_UPSTREAM_ZONE)
+    if (peers->config && rrp->config != *peers->config) {
+        goto busy;
+    }
+#endif      
 
     best = NULL;
     total = 0;
@@ -273,50 +180,46 @@ ngx_http_upstream_get_least_time_peer(ngx_peer_connection_t *pc, void *data)
             continue;
         }
 
-        if (peer->rr->down) {
-	    peer->ema = 0;
+        if (peer->down) {
             continue;
         }
 
-        if (peer->rr->max_fails
-            && peer->rr->fails >= peer->rr->max_fails
-            && now - peer->rr->checked <= peer->rr->fail_timeout)
+#if (NGX_HTTP_UPSTREAM_CHECK)
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0,
+                "get least_conn peer, check_index: %ui",
+                peer->check_index);
+    
+        if (ngx_http_upstream_check_peer_down(peer->check_index)) {
+            continue;
+        }
+#endif
+
+        if (peer->max_fails
+            && peer->fails >= peer->max_fails
+            && now - peer->checked <= peer->fail_timeout)
         {
             continue;
         }
 
-        if (peer->rr->max_conns && peer->rr->conns >= peer->rr->max_conns) {
+        if (peer->max_conns && peer->conns >= peer->max_conns) {
             continue;
         }
 
         /*
-         * select peer with least conns; if there are
-         * multiple peers with the same conns, select
-         * based on mininal avg response
+         * select peer with least response time; if there are
+         * multiple peers with the same response, select
+         * based on round-robin
          */
+
         if (best == NULL
-            || peer->rr->conns * best->rr->weight < best->rr->conns * peer->rr->weight)
+            || GET_AVG_TIME(peer) * best->weight < GET_AVG_TIME(best) * peer->weight)
         {
             best = peer;
             many = 0;
             p = i;
 
-        } else if (peer->rr->conns * best->rr->weight == best->rr->conns * peer->rr->weight) {
-	    if (peer->ema && best->ema) {
-		if (peer->ema < best->ema) {
-		    best = peer;
-		    many = 0;
-		    p = i;
-		} else if (peer->ema == best->ema) {
-		    many = 1;
-		}
-	    } else if (!peer->ema && !best->ema) {
-		many = 1;
-	    } else if (peer->ema && !best->ema) {
-		best = peer;
-		many = 0;
-		p = i;
-	    }
+        } else if (GET_AVG_TIME(peer) * best->weight == GET_AVG_TIME(best) * peer->weight) {
+            many = 1;
         }
     }
 
@@ -342,65 +245,73 @@ ngx_http_upstream_get_least_time_peer(ngx_peer_connection_t *pc, void *data)
                 continue;
             }
 
-            if (peer->rr->down) {
+            if (peer->down) {
                 continue;
             }
 
-	    if (peer->ema * peer->rr->conns * best->rr->weight !=
-		best->ema * best->rr->conns * peer->rr->weight)
-	    {
-                continue;
-            }
-	    
-            if (peer->rr->max_fails
-                && peer->rr->fails >= peer->rr->max_fails
-                && now - peer->rr->checked <= peer->rr->fail_timeout)
+#if (NGX_HTTP_UPSTREAM_CHECK)
+	    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0,
+		    "get least_conn peer, check_index: %ui",
+		    peer->check_index);
+	
+	    if (ngx_http_upstream_check_peer_down(peer->check_index)) {
+		continue;
+	    }
+#endif
+
+	    if (GET_AVG_TIME(peer) * best->weight == GET_AVG_TIME(best) * peer->weight) {
+		if (peer->conns * best->weight > best->conns * peer->weight) {
+		    continue;
+		}
+	    }
+
+            if (peer->max_fails
+                && peer->fails >= peer->max_fails
+                && now - peer->checked <= peer->fail_timeout)
             {
                 continue;
             }
 
-            if (peer->rr->max_conns && peer->rr->conns >= peer->rr->max_conns) {
+            if (peer->max_conns && peer->conns >= peer->max_conns) {
                 continue;
             }
 
-            peer->rr->current_weight += peer->rr->effective_weight;
-            total += peer->rr->effective_weight;
+            peer->current_weight += peer->effective_weight;
+            total += peer->effective_weight;
 
-            if (peer->rr->effective_weight < peer->rr->weight) {
-                peer->rr->effective_weight++;
+            if (peer->effective_weight < peer->weight) {
+                peer->effective_weight++;
             }
 
-            if (peer->rr->current_weight > best->rr->current_weight) {
+            if (peer->current_weight > best->current_weight) {
                 best = peer;
                 p = i;
             }
         }
     }
 
-    best->rr->current_weight -= total;
+    best->current_weight -= total;
 
-    if (now - best->rr->checked > best->rr->fail_timeout) {
-        best->rr->checked = now;
+    if (now - best->checked > best->fail_timeout) {
+        best->checked = now;
     }
-    ngx_log_debug3(NGX_LOG_DEBUG_HTTP, pc->log, 0,
-                   "get least time peer %p c:%ui ema:%ui",
-		   best, best->rr->conns, best->ema);
+    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, pc->log, 0,
+                   "get least time peer %p response %ui", best, GET_AVG_TIME(best));
 
-    pc->sockaddr = best->rr->sockaddr;
-    pc->socklen = best->rr->socklen;
-    pc->name = &best->rr->name;
+    pc->sockaddr = best->sockaddr;
+    pc->socklen = best->socklen;
+    pc->name = &best->name;
 
-    best->rr->conns++;
+    best->conns++;
 
-    rrp->current = best->rr;
-    ltp->current = best;
+    rrp->current = best;
 
     n = p / (8 * sizeof(uintptr_t));
     m = (uintptr_t) 1 << p % (8 * sizeof(uintptr_t));
 
     rrp->tried[n] |= m;
 
-    ngx_http_upstream_rr_peers_unlock(peers->rr);
+    ngx_http_upstream_rr_peers_unlock(peers);
 
     return NGX_OK;
 
@@ -410,9 +321,6 @@ failed:
         ngx_log_debug0(NGX_LOG_DEBUG_HTTP, pc->log, 0,
                        "get least time peer, backup servers");
 
-        rrp->peers = peers->rr->next;
-	ltp->peers = peers->next;
-
         n = (rrp->peers->number + (8 * sizeof(uintptr_t) - 1))
                 / (8 * sizeof(uintptr_t));
 
@@ -420,150 +328,73 @@ failed:
             rrp->tried[i] = 0;
         }
 
-        ngx_http_upstream_rr_peers_unlock(peers->rr);
+        ngx_http_upstream_rr_peers_unlock(peers);
 
-        rc = ngx_http_upstream_get_least_time_peer(pc, ltp);
+        rc = ngx_http_upstream_get_least_time_peer(pc, rrp);
 
         if (rc != NGX_BUSY) {
             return rc;
         }
 
-        ngx_http_upstream_rr_peers_wlock(peers->rr);
+        ngx_http_upstream_rr_peers_wlock(peers);
     }
 
-    ngx_http_upstream_rr_peers_unlock(peers->rr);
+#if (NGX_HTTP_UPSTREAM_ZONE)
+busy:
+#endif
 
-    pc->name = peers->rr->name;
+    ngx_http_upstream_rr_peers_unlock(peers);
+
+    pc->name = peers->name;
 
     return NGX_BUSY;
-}
-
-static inline void
-ngx_http_upstream_least_time_avg(ngx_http_upstream_least_time_peer_t *peer, ngx_msec_t time)
-{
-    peer->ema = (ngx_msec_t)(((double)time * EMA_FACTOR) + ((double)peer->ema * (1 - EMA_FACTOR)));
 }
 
 void
 ngx_http_upstream_free_least_time_peer(ngx_peer_connection_t *pc, void *data,
     ngx_uint_t state)
 {
-    ngx_http_upstream_least_time_peer_data_t  *ltp = data;
+    ngx_http_upstream_least_time_peer_data_t  *d = data;
+    ngx_http_upstream_rr_peer_data_t  *rrp = d->rrp;
 
     time_t				  now;
-    ngx_http_upstream_least_time_peer_t  *peer;
+    ngx_http_upstream_rr_peer_t		 *peer;
     ngx_http_upstream_t			 *u;
     ngx_uint_t				  inflight;
     ngx_event_pipe_t			 *p;
-    ngx_msec_t				  avg;
-    ngx_msec_t				  rsptime;
 
-    peer = ltp->current;
-    u = ltp->request->upstream;
+    peer = rrp->current;
+    u = d->request->upstream;
     p = u->pipe;
 
     ngx_http_upstream_least_time_conf_t *ltcf = ngx_http_conf_upstream_srv_conf(u->conf->upstream, 
 	    ngx_http_upstream_least_time_module);
     
-    ngx_http_upstream_free_round_robin_peer(pc, ltp->rrp, state);
+    ngx_log_debug3(NGX_LOG_DEBUG_HTTP, pc->log, 0,
+                   "free least time peer %ui %ui response %ui", pc->tries, state, u->state->response_time);
+
+    ngx_http_upstream_free_round_robin_peer(pc, rrp, state);
     inflight = !(p->upstream_done || (p->upstream_eof && p->length == -1));
 
-    ngx_http_upstream_rr_peer_lock(ltp->peers->rr, peer->rr);
-
-    rsptime = u->state->response_time;
-
     switch (ltcf->config) {
-    case NGX_TTFB:
-	ngx_http_upstream_least_time_avg(peer, u->state->header_time);
-	rsptime = u->state->header_time;
+    case NGX_LEAST_TIME_HEADER:
+	SET_AVG_TIME(peer, (u->state->header_time + GET_AVG_TIME(peer))/2);
 	break;
-    case NGX_TTLB:
+    case NGX_LEAST_TIME_LAST_BYTE:
 	if (!inflight) {
-	    ngx_http_upstream_least_time_avg(peer, u->state->response_time);
+	    SET_AVG_TIME(peer, (u->state->response_time + GET_AVG_TIME(peer))/2);
 	}
 	break;
-    case NGX_TTLB_INFLIGHT:
-	ngx_http_upstream_least_time_avg(peer, u->state->response_time);
+    case NGX_LEAST_TIME_INFLIGHT_BYTES:
+	SET_AVG_TIME(peer, (u->state->response_time + GET_AVG_TIME(peer))/2);
 	break;
     default:
 	break;
     }
-    ngx_http_upstream_rr_peer_unlock(ltp->peers->rr, peer->rr);
-
-    ngx_log_debug6(NGX_LOG_DEBUG_HTTP, pc->log, 0,
-                   "free least time peer %p c:%ui ema:%ui cfg:%ui i:%ui t:%ui",
-		   peer, peer->rr->conns, peer->ema, ltcf->config, inflight, rsptime);
+    ngx_log_debug4(NGX_LOG_DEBUG_HTTP, pc->log, 0,
+                   "free least time peer %p  avg time %ui config %ui inflight %ui", 
+		   peer, GET_AVG_TIME(peer), ltcf->config, inflight);
 }
-
-#if (NGX_HTTP_UPSTREAM_ZONE)
-static ngx_int_t
-ngx_http_upstream_least_time_init_zone(ngx_shm_zone_t *shm_zone, void *data) 
-{
-    ngx_http_upstream_least_time_peers_t   *peers, *backup;
-    ngx_http_upstream_rr_peers_t	   *rr;
-    ngx_http_upstream_least_time_peer_t    *peer;
-    ngx_http_upstream_least_time_zone_t	   *z = shm_zone->data;
-    ngx_slab_pool_t			   *shpool;
-
-    z->u->peer.data = z->peers->rr;
-    shm_zone->data = z->data;
-
-    shpool = (ngx_slab_pool_t *) shm_zone->shm.addr;
-
-    if (shm_zone->shm.exists) {
-        z->u->peer.data = shpool->data;
-	return NGX_OK;
-    }
-
-    if (z->init(shm_zone, data) == NGX_ERROR) {
-	return NGX_ERROR;
-    }
-
-    rr = z->peers->rr;
-
-    peers = ngx_slab_alloc(shpool, sizeof(ngx_http_upstream_least_time_peers_t));
-    if (peers == NULL) {
-	return NGX_ERROR;
-    }
-    ngx_memzero(peers, sizeof(ngx_http_upstream_least_time_peers_t));
-
-    peer = ngx_slab_alloc(shpool, sizeof(ngx_http_upstream_least_time_peer_t) * rr->number);
-    if (peer == NULL) {
-	return NGX_ERROR;
-    }
-    ngx_memzero(peer, sizeof(ngx_http_upstream_least_time_peer_t) * rr->number);
-
-    peers->peer = peer;
-    ngx_http_upstream_least_time_ref_peers(peers, rr);
-
-    z->u->peer.data = z->peers = peers;
-    shm_zone->data = z;
-    shpool->data = peers;
-    rr = z->peers->rr->next;
-
-    if (!rr) {
-	return NGX_OK;
-    }
-
-    backup = ngx_slab_alloc(shpool, sizeof(ngx_http_upstream_least_time_peers_t));
-    if (backup == NULL) {
-	return NGX_ERROR;
-    }
-    ngx_memzero(backup, sizeof(ngx_http_upstream_least_time_peers_t));
-
-    peer = ngx_slab_alloc(shpool, sizeof(ngx_http_upstream_least_time_peer_t) * rr->number);
-    if (peer == NULL) {
-	return NGX_ERROR;
-    }
-    ngx_memzero(peer, sizeof(ngx_http_upstream_least_time_peer_t) * rr->number);
-
-    backup->peer = peer;
-    ngx_http_upstream_least_time_ref_peers(backup, rr);
-    peers->next = backup;
-
-    return NGX_OK;
-}
-#endif
 
 static char *
 ngx_http_upstream_least_time(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
@@ -585,14 +416,14 @@ ngx_http_upstream_least_time(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 
     if (cf->args->nelts == 2 && value[1].len == (sizeof("header") - 1) && 
 	!ngx_strncmp(value[1].data, (u_char *)"header", sizeof("header") - 1)) {
-	ltcf->config = NGX_TTFB;
+	ltcf->config = NGX_LEAST_TIME_HEADER;
     } else if (value[1].len == (sizeof("last_byte") - 1) &&
 	       !ngx_strncmp(value[1].data, (u_char *)"last_byte", sizeof("last_byte") - 1)) {
 	if (cf->args->nelts == 3 && value[2].len == (sizeof("inflight") - 1) &&
 	    !ngx_strncmp(value[2].data, (u_char *)"inflight", sizeof("inflight") - 1)) {
-	    ltcf->config = NGX_TTLB_INFLIGHT;
+	    ltcf->config = NGX_LEAST_TIME_INFLIGHT_BYTES;
 	} else {
-	    ltcf->config = NGX_TTLB;
+	    ltcf->config = NGX_LEAST_TIME_LAST_BYTE;
 	}
     }
 
--- ngx_http_upstream_least_time.h
+++ ngx_http_upstream_least_time.h
@@ -14,51 +14,18 @@
 #include <ngx_http.h>
 
 enum {
-    NGX_TTFB = 1,
-    NGX_TTLB = 2,
-    NGX_TTLB_INFLIGHT = 3,
+    NGX_LEAST_TIME_HEADER = 1,
+    NGX_LEAST_TIME_LAST_BYTE = 2,
+    NGX_LEAST_TIME_INFLIGHT_BYTES = 3,
 };
 
 typedef struct {
     ngx_uint_t config;
 } ngx_http_upstream_least_time_conf_t;
 
-typedef struct ngx_http_upstream_least_time_peer_s   ngx_http_upstream_least_time_peer_t;
-
-#define REQS_RANGE	    16
-#define EMA_FACTOR	    (2.0/(1.0 + REQS_RANGE))
-
-struct ngx_http_upstream_least_time_peer_s {
-    ngx_http_upstream_rr_peer_t		 *rr;
-    ngx_http_upstream_least_time_peer_t  *next;
-    ngx_msec_t				  ema;
-
-    NGX_COMPAT_BEGIN(32)
-    NGX_COMPAT_END
-};
-
 typedef struct ngx_http_upstream_least_time_peers_s  ngx_http_upstream_least_time_peers_t;
 
-struct ngx_http_upstream_least_time_peers_s {
-    ngx_http_upstream_rr_peers_t	  *rr;
-
-    ngx_http_upstream_least_time_peers_t   *next;
-    ngx_http_upstream_least_time_peer_t    *peer;
-};
-
-#if (NGX_HTTP_UPSTREAM_ZONE)
 typedef struct {
-    ngx_http_upstream_least_time_peers_t *peers;
-    ngx_http_upstream_main_conf_t        *cf;
-    ngx_http_upstream_srv_conf_t	 *u;
-    ngx_shm_zone_init_pt		  init;
-    void                                 *data;
-} ngx_http_upstream_least_time_zone_t;
-#endif
-
-typedef struct {
-    ngx_http_upstream_least_time_peers_t   *peers;
-    ngx_http_upstream_least_time_peer_t    *current;
     ngx_http_request_t			   *request;
     ngx_http_upstream_rr_peer_data_t	   *rrp;
 } ngx_http_upstream_least_time_peer_data_t;
